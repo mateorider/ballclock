79319770e4d90d7c33d18a4adbad34659c693c22
Matt Rider <matt.rider@octanner.com>
2018-02-19 01:56:08 -0700

    Write steps for mode1

---
+++ modes/modes.go
@@ -0,0 +1,19 @@
+package modes
+
+// Mode1 takes a single parameter specifying the number of balls and reports the number of balls 
+// given in the input and the number of days (24-hour periods) which elapse before the clock returns
+// to its initial ordering. 
+// Returns days int, seconds float64
+func Mode1 (iBalls int) (int, float64) {
+    // start timer
+
+    // create a two instances of BallClock, one to iterate and one to compare to
+
+    // Offset the states
+
+    // Until ballClock matches the initialClockState, add minutes
+    
+    // stop the timer and calculate days
+
+    return 1, 1.0
+}
